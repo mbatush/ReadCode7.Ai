0174e982dafb3440675573f3d53366b40f2e6094
MichaelBatush <mbatusha@mail.yu.edu>
2025-02-15 23:01:30 -0500

    back

--- src/components/Axios.js
+++ src/components/Axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const AxiosInstance = axios.create({
-  baseURL: "http://54.162.156.134", // ✅ Ensure this is correct
+  baseURL: "http://127.0.0.1:8000", // ✅ Using local Django API
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",
